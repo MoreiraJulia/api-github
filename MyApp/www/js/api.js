25d3cfc7460bb84e1794a2087cff5e94c65af63e
Júlia Aparecida Moreira <julia.aparecida25@gmail.com>
2022-05-25 22:05:18 -0300

    Atualizações

---
+++ MyApp/www/js/api.js
@@ -0,0 +1,29 @@
+
+usuarios = ['gutoffline', 'taynaradb', 'SantiagoVinicius', 'MoreiraJulia'];
+
+function BuscarGithub(){
+function imprimir(individual) {
+    $.ajax({
+        type: "GET",
+        url: 'https://api.github.com/users/' + individual,
+         success: (function(user){
+             console.log(user)
+             let nome = user.name
+             let login = user.login
+             let foto = user.avatar_url
+             let twitter = user.twitter_username
+             let github = user.url
+             let medium = user.blog
+             let instuicao = user.company
+             let pais = user.location
+             document.getElementById("informacao-completo").innerHTML += `<div class="espaco"><div class="col-md-5"><img src="${foto}" width="150"></div> <div class="col-md-7"> ${nome} (${login}) <br> <a href="${twitter}"><i class="bi bi-twitter"></i></a> <a href="${github}"><i class="bi bi-github" width="100"></i></a> <a href="${medium}"><i class="bi bi-medium"></i></a> <br> ${instuicao} - ${pais}</div>`
+        })
+    })
+}
+
+usuarios.forEach(imprimir);
+}
+
+document.getElementById("buscar").addEventListener("click", function() {
+    BuscarGithub();
+})
